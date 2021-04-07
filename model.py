59193661910ecb6f2d9000954dd8383c5cff7536
MaressaMachado <75862640+MaressaMachado@users.noreply.github.com>
2021-04-07 19:32:40 -0300

    Contribuicao erro

--- model.py
+++ model.py
@@ -15,6 +15,8 @@ class BuscaAPI:
         mus = f'mus={self.musica}'
         url = self.url + f'search.php?{art}&{mus}'
         info = requests.get(url=url)
+        if info.status_code != 200:
+            raise Exception(f"ERRO [{info.status_code}]\nREQUEST [{url}]")
         info_j = info.json()
         letra_musica = info_j['mus'][0]['text']
         return letra_musica
@@ -27,6 +29,8 @@ class BuscaAPI:
         mus = f'mus={self.musica}'
         url = self.url + f'search.php?{art}&{mus}'
         info = requests.get(url=url)
+        if info.status_code != 200:
+            raise Exception(f"ERRO [{info.status_code}]\nREQUEST [{url}]")
         info_j = info.json()
         traducao_musica = info_j['mus'][0]['translate'][0]['text']
         return traducao_musica
@@ -38,6 +42,8 @@ class BuscaAPI:
         url = self.url + f'rank.php?type={tipo_de_rank}&period={periodo}' \
                          f'&scope={escopo}&limit={limite}'
         info = requests.get(url=url)
+        if info.status_code != 200:
+            raise Exception(f"ERRO [{info.status_code}]\nREQUEST [{url}]")
         info_rank = info.json()
         rank = info_rank[tipo_de_rank][periodo]  # [escopo]
         return rank

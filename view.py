2d357b1a31f58bdc4d03ae0564ce80c01099eef2
JP NUNES <jpnune@gmail.com>
2021-04-08 15:34:41 -0300

    teste

--- view.py
+++ view.py
@@ -1,7 +1,8 @@
 class View:
     def introducao(self):
-        #teste merge
-        
+        '''
+        teste_merge
+        '''
         print('Seja bem vindo a API do Vagalume!'
               '\nAqui você pode encontrar algumas informações do mundo d'
               'a música!!!')
